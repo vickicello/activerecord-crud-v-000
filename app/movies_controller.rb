03076c720b1ee5136d61343eb409e701a6a7c906
Vicki Hamm <vicki.cello@gmail.com>
2018-08-16 22:53:08 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -43,7 +43,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.all.first(id: 1)
+  Movie.all.find_by(id: 1)
 end
 
 def can_find_by_multiple_attributes
